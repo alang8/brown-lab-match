a1549fb1c31f993e0c20d234f95288dc68f84713
suivo2001 <73476329+suivo2001@users.noreply.github.com>
2021-05-23 11:12:55 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-module.exports = {
-    PORT: process.env.PORT,
-    DB_NAME: process.env.DB_NAME,
-    DB_URL: process.env.DB_URL,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_HOST: process.env.DB_HOST
-};
