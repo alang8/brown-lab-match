603bcd1b19ab8b2f37fea0bcff8e085e1cbcf85f
Alexander Ding <alexander.jf.ding@gmail.com>
2021-06-18 22:21:36 -0400

    Deploy branch

--- server/routes/api.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const express = require('express');
-const labRouter = require('./lab_information')
-
-let apiRouter = express.Router()
-    .use('/', labRouter)
-
-module.exports = apiRouter;
